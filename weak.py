a4299145c20a71e1412075ca369713fd441878c1
MSameer-92 <iamsameerai65@gmail.com>
2026-05-05 21:21:41 +0500

    first commit

--- weak.py
+++ weak.py
@@ -1,26 +1,3 @@
-# from selenium import webdriver
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.common.keys import Keys
-# import time
-
-
-# driver=webdriver.Chrome()
-
-# driver.get("https://www.google.com")
-
-# time.sleep(2)
-
-# search_box=driver.find_element(By.NAME,"q")
-
-# search_box.send_keys("https://www.wikipedia.org/")
-
-# search_box.send_keys(Keys.RETURN)
-
-# time.sleep(3)
-
-# driver.quit()
-
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -82,4 +59,3 @@ except Exception as e:
 finally:
     driver.quit()
     print("Browser closed.")
-
